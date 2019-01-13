601b4fe7eb2aed444ac9195ba354b99ef0d3ebf7
lawki <prakharpalpfdr@gmail.com>
2019-01-14 00:33:26 +0530

    add constants file to easily manage global variables
    Former-commit-id: b61378bf090e9726d23d021483b20c7f275f5a19 [formerly 20686dc31399c2a3521e1b31838426f2be7b4858]
    Former-commit-id: b203d05422afa094b93729570cef0b683e86b351
    Former-commit-id: 041c663017ad1bb3fb3f03acce7ebe4d911af759 [formerly 851b1ebe28af769a4b7c27225756f0d5fa54f2da] [formerly 58f75d147dc16e0d55b1a83ee1a74629bd29986d [formerly 30d83c09a847847b97f61a2077fd7829f24c6063]]
    Former-commit-id: 58f75d147dc16e0d55b1a83ee1a74629bd29986d
    Former-commit-id: 71532dcd83e75c6bdf98eeb8c2d6759c054fd862
    Former-commit-id: d6c237f9b0c770dc6fca376031467fc2bf4b7127 [formerly a03e0866ccfed305917d94cdd0bb5f2e9d2aa873]
    Former-commit-id: 926a74d4a56f5c4a1250a2eb8fecaeaf4d148aa2

---
+++ constants.js
@@ -0,0 +1,7 @@
+module.exports = {
+    FAST_MODE: 'fast',
+    SLOW_MODE: 'slow',
+    LEGACY_MODE: 'legacy',
+    ACCEPTABLE_MEDIA_EXTENSIONS:['.mp4','.webm','.png','.jpg','.jpeg' ,'.bmp','.gif','.ogg','.mkv'],
+    SHOW_LOGS: 'show'
+}
