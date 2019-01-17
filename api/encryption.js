3aa74f42ace6873853ff7cb472987fd5e21ddac2
lawki <prakharpalpfdr@gmail.com>
2019-01-18 00:21:47 +0530

    change algorithm to better encryption algorithm(to remove unwanted warnings)
    Former-commit-id: 48172bba1444dbcd3c033e224e39f114c2df7c6e [formerly 952552774621da89c90da4442755cf056d9e135d]
    Former-commit-id: a5e4b99c091969c8039cb1f41bf476338afd1a52
    Former-commit-id: 21196e9687f9f1950487d552d6864d89e85e840f [formerly 4c1b90a9f0ae5e1b0e482b875aca3bf01f33c70c] [formerly 2a2a8c78e78148d437d68224f75200dcc8269398 [formerly ec26e1435b0917c1efb71205063eb2a6dde1047a]]
    Former-commit-id: 2a2a8c78e78148d437d68224f75200dcc8269398
    Former-commit-id: 8d12591955aec8263ff01ba1eef02e94d2b1ef16
    Former-commit-id: 0eb0befbb924dfe2924944d2a564edfbc4cc5562 [formerly e135cc88b64b86c9e27fc9e68648e8e0e68c6e44]
    Former-commit-id: e45b9d55ca37f6fbc7c011fc3f3be3bf1b86c810

--- api/encryption.js
+++ api/encryption.js
@@ -1,26 +1,65 @@
+const utils = require('./utils/utils');
 var crypto = require('crypto');
-    algorithm = 'aes-192-ctr';
+    algorithm = 'aes-256-cbc';
     password = 'd6F3Efeq';
 var encrypt = function (text){
   var cipher = crypto.createCipher(algorithm,password)
   var crypted = cipher.update(text,'utf8','hex')
   crypted += cipher.final('hex');
+  let showLogs = utils.ifShowLogs();
+  console.log('showLogs? ',showLogs);
+  utils.ifShowLogs() && console.log(`encrypt():: input=${text}, output=${crypted}`);
   return crypted;
 }
 
 var decrypt = function (text){
     try
     {
+      utils.ifShowLogs() && console.log('trying to decrypt ',text);
       var decipher = crypto.createDecipher(algorithm,password)
       var dec = decipher.update(text,'hex','utf8')
       dec += decipher.final('utf8');
       return dec;
     }
     catch(err){
-        //console.log(err);
         return null;
     }
 }
+if(require.main==module){
+  console.log(`${process.argv[1]} is being run as module.`);
+  /**
+   * @Test 
+   * whether encryption works
+   */
+  let payload = process.argv[2] || "lorem ipsum idt";
+  let encryptedPayload, decryptedPayload;
+  try{
+      encryptedPayload = encrypt(payload);
+  }catch(err){
+      console.log("Encryption failed: ",err);
+      return;
+  }
+  /**
+   * @Test 
+   * whether decryption works
+   */
+  try{
+      decryptedPayload = decrypt(encryptedPayload);
+  }catch(err){
+      console.log("Decryption failed: ",err);
+      return;
+  }
+  if(payload !== decryptedPayload){
+    console.log("Whole module failed.");
+  }else{
+    console.log(`Original data = ${payload}, 
+               encrypted payload = ${encryptedPayload},
+               decrypted payload = ${decryptedPayload}`);
+    console.log("SUCCESS!!");
+  }
+
+}
+
 module.exports = {
   encrypt:encrypt,
   decrypt:decrypt,
