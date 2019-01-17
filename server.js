3f023849332dffe4c30e99a6adaa3e4f3ce703f3
lawki <prakharpalpfdr@gmail.com>
2019-01-17 23:40:27 +0530

    cleanup Jade rendering code, @TODO testing whether it works
    Former-commit-id: a0c4a5cfcb1bdee8f7d77473de37691fcd013061 [formerly 0ed213eb9ef7541ed6fbd52caa3c0f09f6fd71af]
    Former-commit-id: 038aed8bb5055f59ccf43d9fa56137ad6778309c
    Former-commit-id: e4be38daba0ff88cd269b3475c5793694eaa02cd [formerly 9a1b4142a6ba07b036b70bfa4c01b64bbe56bf73] [formerly 17e78cb6fcbc497e5c918d3d61bcc03a2f442e94 [formerly f8b4e1e14fb6731039a7116d58cc5a9c42a82eaa]]
    Former-commit-id: 6d6daf4ebba1d544ee9bdfd57dfe6feab0c7883a [formerly 17e78cb6fcbc497e5c918d3d61bcc03a2f442e94]
    Former-commit-id: a3615ffebac1bf05a29c98ea88f868fa8ea85db2
    Former-commit-id: 80cd4958ba351d0029179f6a58523d15e3722cac [formerly 1d217f298697a20c179ec591d786629ddd41f851]
    Former-commit-id: 6a592a90d3ef87d745324d9ff223ac33189159e1

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const constants = require('./constants');
 const fs = require('fs');
 const cors = require('cors');
-const sendSeekable = require('send-seekable');
 var {argv} = require('process');
 require('jade');
 require('amdefine');
@@ -14,38 +12,22 @@ require('amdefine');
 
 var filePath = require('./config.js');
 var vidAPI = require('./api/router-videos');
-const {encrypt, decrypt} = require('./api/encryption.js');
+const utils = require('./api/utils/utils');
 if(argv[2])
     filePath.path = argv[2];
 console.log("The base directory is :"+filePath.path);
 
 var PORT = argv[3]?argv[3]:3000;
-
-//console.log('Port for the server is :'+PORT);
-
-//const getDirs = source => readdirSync(source).map(name => path.join(source, name)).filter(isDirectory);
-//const getFiles = source => readdirSync(source).map(name => join(source, name)).filter(isFile))
+if(utils.ifShowLogs())
+    console.log('Port for the server is :'+PORT);
 
 var app = express();
 app.use(cors());
-app.use(function(req,res,next){
-    //url = req.url;
-    //console.log('requested url: '+url);
-    // if(url.search('.flv')!=-1){
-    //     res.contentType('video/x-flv');
-    // }
-    next();
-})
+app.use(logger('tiny'));
 app.use('/api',vidAPI);
-// app.use(sendSeekable);
-
-//add jade templating engine
-app.get('/flv',(req,res)=>{
-    res.sendFile(path.join(__dirname,'public','flv-test.html'));
-})
-app.get('/mkv',(req,res)=>{
-    res.sendFile(path.join(__dirname,'public','mkv.html'));
-})
+/**
+ * add jade templating engine 
+ * */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -59,99 +41,73 @@ app.get('/',function(req,res){
         res.sendFile('index.html');
 });
 
+/** render jade templates for particular URL schemes */
 app.get('/get/:file',function(req,res){
-    //console.log('encrypted file name: '+req.params.file);
-    //res.send('get/file request.');
-    //console.log(JSON.stringify(req.headers));
     var file = decrypt(req.params.file);
-    if(!file)
-    {
+    if(!file){
         res.status(404).send('Error decrypting the file name');
+        return;
     }
-    console.log("decrypted file name :"+file);
+    utils.ifShowLogs() && console.log("decrypted file name :"+file);
     var f = path.join(filePath.path,file);
-    console.log('requested file path is '+f);
-    if(!fs.existsSync(f))
-    {
-        res.send('The file \"'+file+'\" doesn\'t exist on this server');
+    utils.ifShowLogs() && console.log('requested file path is '+f);
+    
+    if(!fs.existsSync(f)){
+        res.status(404).send('The file \"'+file+'\" doesn\'t exist on this server');
     }
-    else
-    {
-        if(fs.statSync(f).isFile())
-        {
-            var extension = path.extname(f);  //gets the extension of the file name
-            switch(extension) {
-            case '.mp4':
-            case '.webm':
-            case '.png':
-            case '.jpg':
-            case '.jpeg':
-            case '.bmp':
-            case '.gif':
-                        //const buf  = new Buffer('Weave a circle round him thrice');
-                       //res.render('video',{title:file,file:'http://localhost:3000/'+file});
-                       res.redirect('/'+file);
-                       //res.send(buf);
-                       break;
-            default:
-                       res.send('The video format '+extension+" for file \""+file+"\" isn't supported");
+    else{
+        if(fs.statSync(f).isFile()){
+            var extension = path.extname(f);
+            if(constants.includes(extension)) {
+                res.redirect('/'+file);
+            }else{
+                res.status(404).send('The video format '+extension+" for file \""+file+"\" isn't supported");
             }
-
         }
-        else if(fs.stat(f).isDirectory())
-        {
+        else if(fs.stat(f).isDirectory()){
             var dir = decrypt(req.params.file);
-            console.log('you accessed '+dir);
+            res.status(404).send("You tried accessing "+dir);
         }
-        else
-        {
-            res.send('some error occured.');
+        else{
+            res.status(404).send('some error occured.');
         }
-
     }
-})
+});
 
 app.get('/:dir',(req,res)=>{
-    if(!req.params.dir)
-    {
-        res.send('Got a null/malformed directory');
+    if(!req.params.dir){
+        res.status(404).send('Got a null/malformed directory');
     }
     var dir = decrypt(req.params.dir);
-    if(!dir)
-    {
+    if(!dir){
         res.status(404).send('Error while decrypting the file name');
     }
-    console.log('requested directory: '+dir);
+    utils.ifShowLogs() && console.log('requested directory: '+dir);
     var d = path.join(filePath.path,dir);
-    if(fs.existsSync(d))
-    {
-        if(fs.statSync(d).isDirectory())
-        {
-            var files = [];//getFiles(filePath.path);
-            var directories =  [];//getDirs(filePath.path);
+    if(fs.existsSync(d)){
+        if(fs.statSync(d).isDirectory()){
+            var files = [];
+            var directories =  [];
             var filesEncoded = [];
             var directoriesEncoded = [];
             var contains = fs.readdirSync(d);
             contains.forEach((file)=>{
                var stat = fs.statSync(path.join(filePath.path,dir,file));
-               if(stat.isFile())
-               {
+               if(stat.isFile()){
                    files.push(file);
                    filesEncoded.push(encrypt(path.join(dir,file)));
                }
-               else
-               {
+               else{
                    directoriesEncoded.push(encrypt(path.join(dir,file)));
                    directories.push(file);
                }
             });
-            console.log("Priting the files");
+            utils.ifShowLogs() && console.log("Priting the files");
             res.render('listing.jade',{title:'File listing',files:files,dirs:directories,filesEncoded:filesEncoded,dirsEncoded:directoriesEncoded});
         }
         else{
-
+            /**@TODO ? left out from the previous commit */
         }
-
     }
     else{
         res.header(404).send('The URL is invalid');
