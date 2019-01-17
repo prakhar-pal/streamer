dae42221c1f1fd98e4202b849ccc3bcbf1eeae9a
lawki <prakharpalpfdr@gmail.com>
2019-01-18 00:28:02 +0530

    add logic to recursively calculate tree structure in files system. @TODO; make a package
    Former-commit-id: 733e68e58e3edcebb8a54234ecd5c74b104cee68 [formerly 227dac6e3d1b38783a2ba7056b7bf74ce23ed495]
    Former-commit-id: c9e772bd14514965b83bf2a29309cf237bc67b11
    Former-commit-id: 103e7efa16ea9af1fd12428ce65e8a182963c89a [formerly a91586e514dfa07de95c315db11650f2f555822e] [formerly 0ecc5e72058592dd88112e6552d820fcf6e8db3c [formerly edee3f69d8d2bb8c7d81ff3ed9ce34fe08ced201]]
    Former-commit-id: 233c1efb76d4c033877b850194b5821a6ccde97d [formerly 0ecc5e72058592dd88112e6552d820fcf6e8db3c]
    Former-commit-id: 0df343de9e5a92dd57045868ecbdd24c766d6752
    Former-commit-id: d46d84cdfebd824ae11fd28c3d26be6d71f9dc05 [formerly 0864929117d193e05c7f6c5195d9cca3f4291ede]
    Former-commit-id: 53290cfb8b8e167c60b51599f87d23b30a08e519

--- api/directory_trees.js
+++ api/directory_trees.js
@@ -1,51 +1,79 @@
+/**
+ * Author: Prakhar Pal(https://github.com/lawki)
+ * Created On: 26-09-2018
+ */
 const fs = require('fs');
 const path = require('path');
 
-const print = function(data){console.log(data);}
+/*
+	This function prints messages for fact checking. Use this in place of console.log() to make those messages 
+	easily toggable.
+*/
+const print = function(data){
+	// console.log(data);
+}
 const listFilesAndDirs = function(dir)
 {
+	print('inside listFilesAndDirs():');
 	let out = {
 		files:[],
 		directories:[]
 	};
 	let fad = fs.readdirSync(dir); /*fad=files and directory*/
-	fad.forEach(
-		function(fName)
+	fad.forEach(function(fName){
+		let f = path.join(dir,fName);
+		let stat = null;
+		try
 		{
-			let f = path.join(dir,fName);
-			let stat = null;
-			try
-			{
-				stat = fs.statSync(f);
-			}catch(e)
-			{
-				return;
-			}
-			if(stat.isDirectory())
-			{
-				out.directories.push(fName);
-			}
-			else
-			{
-				out.files.push(fName);
-			}
-		});
+			stat = fs.statSync(f);
+		}catch(e)
+		{
+			return;
+		}
+		if(stat.isDirectory())
+		{
+			out.directories.push(fName);
+		}
+		else
+		{
+			out.files.push(fName);
+		}
+	});
 	return out;
 }
 /*
 INPUT:
 	1)dir: absolute directory path for which the tree is to be made
 	2)ignoreWords: an array of words; if contained in a directory, that directory will be ignored.
-		e.g. node_modules, android project folders etc. might not be desired at the front end.
+		e.g. node_modules, android project folders etc. might not be desired as output.
 */
 const makeTree = function(dir,ignoreWords)
 {
-	Object.prototype.toString.call(ignoreWords)=='[object Array]' && ignoreWords.forEach((ignoreWord)=>
+	if(!Array.isArray(ignoreWords)){
+		throw new Error("ignoreWords is not an array.");
+		return;
+	}
+	print("inside makeTree()");
+	let areIgnoreWordsPresent = false;
+	let _this = this;
+	let cDir = path.basename(dir); //Get current working directory. E.g. for /home/user/Downloads it is 'Downloads'
+	print("current directory is :"+dir);
+	print("Words to ignore are:"+ignoreWords);
+	for(var i=0;i<ignoreWords.length;i++)
 	{
-		if(dir.indexOf(ignoreWord)!=-1)
+		let word = ignoreWords[i];
+		if(cDir.includes(word))
+		{
+			print("found pattern for "+word+" in dir "+cDir);
+			_this.areIgnoreWordsPresent = true;
 			return {};
-	});
-	//print('root is:'+dir);
+		}
+	}
+	if(areIgnoreWordsPresent){
+		console.log(`ERROR for ${dir}`);
+		return ;
+	}
+	//print('listing files for:'+dir)
 	let root = listFilesAndDirs(dir);
 	root.directories && root.directories.forEach(function(directory)
 	{
@@ -53,19 +81,33 @@ const makeTree = function(dir,ignoreWords)
 	})
 	return root;
 }
-let currentDir = process.cwd();
-try{
-	let ignore = [
-		'node_modules'
-	];
-	let outputTree = JSON.stringify(makeTree(currentDir),null,2);
-	// fs.writeFile(path.join(currentDir,'tree.txt'),outputTree,function(err)
-	// {
-	// 	print(err);
-	// })
-	print(outputTree);
+/*
+TEST CODE.
+executed only when called as main module.
+NOT executed only when called with require.
+
+*/
+if(require.main==module){
+	let currentDir = process.cwd();
+	try{
+		let ignore = [
+			'node',
+			'.cache',
+			'.git'
+		];
+		//ignore = {};
+		print(`root directory is ${currentDir}`);
+		let outputTree = JSON.stringify(makeTree(currentDir,ignore),null,2);
+		// fs.writeFile(path.join(currentDir,'tree.txt'),outputTree,function(err)
+		// {
+		// 	print(err);
+		// })
+		console.log(outputTree);
+	}
+	catch(e){
+		//print(JSON.stringify(makeTree(currentDir),null,2));
+		console.log('ERROR!: '+e);
+	}
 }
-catch(e){
-	//print(JSON.stringify(makeTree(currentDir),null,2));
-	console.log('ERROR!: '+e);
-}
+
+module.exports = makeTree;
